b0a0e27a72bab7aa414826a5c14aff481d660832
Pinness <adamhappinessjw>
2024-08-09 04:34:34 +0100

    Added route `/categories` to fetch and display a list of quiz categories.

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class User(db.Model):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
 
 @app.route('/login', methods=["POST"]) #snce we're sending its post
@@ -132,6 +132,104 @@ def dashboard():
     if "username" in session:
         return render_template('dashboard.html', username=session["username"])
     return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+
+class UserResponse(db.Model):
+    __tablename__ = 'user_response'
+
+    user_response_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'), nullable=False)
+    selected_answer_id = db.Column(db.Integer, db.ForeignKey('answers.answer_id'), nullable=False)
+    is_correct = db.Column(db.Boolean, nullable=False)
+    
+    question = db.relationship('Question', back_populates='user_responses')
+    selected_answer = db.relationship('Answer')
+    user = db.relationship('User')
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    category_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category_name = db.Column(db.String(50), unique=True, nullable=False)
+    category_description = db.Column(db.Text)
+    
+    quizzes = db.relationship('Quiz', back_populates='category')
+    
+    def __repr__(self):
+        return f'<Category {self.category_name}>'
+
+
+
+
+class Quiz(db.Model):
+    __tablename__ = 'quizzes'
+    quiz_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_title = db.Column(db.String(100), nullable=False, unique=True)
+    quiz_description= db.Column(db.Text)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.category_id"), nullable=False)
+    
+    category = db.relationship('Category', back_populates='quizzes')
+    questions = db.relationship('Question', back_populates='quiz')
+
+
+    def __repr__(self):
+        return f'<Quiz {self.quiz_title}>'
+
+
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+    
+    question_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.quiz_id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    question_type = db.Column(db.String(50), nullable=False)
+    
+    quiz = db.relationship('Quiz', back_populates='questions')
+    answers = db.relationship('Answer', back_populates='question')
+    user_responses = db.relationship('UserResponse', back_populates='question')
+
+
+    def __repr__(self):
+        return f'<Question {self.text}>'
+
+
+class Answer(db.Model):
+    __tablename__ = 'answers'
+    
+    answer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'))
+    ans_text = db.Column(db.Text, nullable=False)
+    is_correct = db.Column(db.Boolean, default=False)  # Add this line
+
+    question = db.relationship('Question', back_populates='answers')
+
+    def __repr__(self):
+        return f'<Answer {self.is_correct}>'
+
+
+
+# class UserResponse(db.Model):
+#     __tablename__ = 'user_response'
+# 
+#     user_response_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'), nullable=False)
+#     selected_answer_id = db.Column(db.Integer, db.ForeignKey('answers.answer_id'), nullable=False)
+#     is_correct = db.Column(db.Boolean, nullable=False)
+#     
+#     question = db.relationship('Question', back_populates='user_responses')
+#     selected_answer = db.relationship('Answer')
+#     user = db.relationship('User')
+
 
 
 if __name__ == "__main__":
